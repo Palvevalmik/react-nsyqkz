97b67ef971b8b4fb4677d85fc7a0117ec4abccb7
Palvevalmik <58813855+Palvevalmik@users.noreply.github.com>
2022-11-22 11:58:19 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+import Person from './components/Person';
+
+export default function App() {
+  return (
+    <div>
+      <Person />
+    </div>
+  );
+}
---
+++ src/components/Person.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = {
+      name: "Valmik Palve",
+      age: 29,
+    };
+  }
+
+  nameChangeHandler() {
+    //alert("hello!");
+    console.log(this.state);
+    this.setState({ name: "shubham", age: 28 }, () => {
+      console.log(this.state);
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>
+          <i>
+            Dear {this.state.name}, {this.state.age} years, Welcome to pune!
+          </i>
+          <br />
+          <button onClick={() => this.nameChangeHandler()}>Click me!</button>
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Person;
